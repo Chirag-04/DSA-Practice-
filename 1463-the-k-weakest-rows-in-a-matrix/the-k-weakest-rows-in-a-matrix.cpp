cdc3378a8982bca03cb07f93a768598fe4efde23
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-06 08:21:42 +0530

    Time: 10 ms (44.95%) | Memory: 12.7 MB (100.00%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+   // approach 1 : use simple brute force
+   // anything realted to sorted => binary
+   int findOne(vector<vector<int>>& mat , int i ){
+      int lo = 0 ;
+      int hi = mat[i].size()-1;
+      int ans = -1;
+      while(lo<=hi){
+        int mid = lo+(hi-lo)/2;
+        if(mat[i][mid] ==1 ){
+            ans =  mid;
+            lo =  mid+1;
+        }
+        else {
+            hi = mid-1;
+        }
+      }
+      return ans ;
+   }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        // can be solved using min heap
         int n = mat.size();
         vector<pair<int,int>>v;
         for(int i=0; i<mat.size(); i++){
-            int ct = 0;
-            for(int j=0 ; j<mat[i].size();j++){
-                if(mat[i][j] ==1) ct++;
-                else break;
-            }
+            int ct  =  findOne(mat , i);
+            if(ct<0)ct=0;
+            else ct++;
             v.push_back({ct , i});
         }
         sort(v.begin() , v.end());
