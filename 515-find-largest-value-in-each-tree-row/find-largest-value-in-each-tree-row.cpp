30c97ba9392c142609814c350882bde45a9c52e6
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-11 10:21:31 +0530

    Time: 11 ms (38.10%) | Memory: 20.7 MB (24.05%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -17,16 +17,16 @@ public:
         q.push(root);
         vector<int>ans;
         while(!q.empty()){
-            vector<int>temp;
+            int maxi = INT_MIN;
             int sz = q.size();
             for(int i=0; i <sz ; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                temp.push_back(node->val);
+                maxi = max(maxi , node->val);
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            int maxi =*max_element(temp.begin(),temp.end());
+            // int maxi =*max_element(temp.begin(),temp.end());
             ans.push_back(maxi);
         }
         return ans ;
