80730bc2e11a246f2ff71e4d2ce856397845159e
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-11 16:46:57 +0530

    Time: 159 ms (66.34%) | Memory: 95.6 MB (37.22%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -13,18 +13,16 @@ public:
         ll ans  = 0 ;
         ll sum = 0 ;
         priority_queue<int,vector<int>,greater<int>>pq;
-        for(int  i=0 ; i< n ;i++){
-            if(i<=k-1){
-                sum+=v[i].second;
-                pq.push(v[i].second);
-                ans =  (sum)*1LL*v[i].first;
-            }
-            else{
+        for(int i=0 ;i<=k-1 ; i++){
+            sum+=v[i].second;
+            pq.push(v[i].second);
+        }
+        ans =  sum*1LL*v[k-1].first;
+        for(int  i=k ; i< n ;i++){ 
                 sum+=v[i].second-pq.top();   
                 pq.pop();
                 ans = max(ans , sum*1LL*v[i].first);
                 pq.push(v[i].second);
-            }
         }
         return ans ;
     }
