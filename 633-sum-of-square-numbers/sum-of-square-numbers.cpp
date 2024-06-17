af43512073daa4d67f0ea8b394279b5f99c0f004
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-17 18:52:42 +0530

    Time: 3 ms (68.00%) | Memory: 7.1 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,26 +2,13 @@ class Solution {
 public:
     using ll = long long ;
     bool judgeSquareSum(int c) {
-        ll l =  0;
-        ll h  = ceil((double)sqrt(c));
-        
-        for(ll a=l ; a<=h ; a++){
-            ll b = c -(a*a);
-            ll lo = a;
-            ll hi = h;
-            while(lo<=hi){
-                ll mid = lo +(hi-lo)/2;
-                if(mid*mid == b){
-                    return true;
-                }
-                else if(mid*mid > b){
-                    hi = mid-1;
-                }
-                else{
-                    lo = mid+1;
-                }
-            }
-        }
-        return false;
+     ll lo = 0 ;
+     ll hi = ceil((double)sqrt(c));
+     while(lo<=hi){
+        if(lo*lo + hi*hi == c) return true;
+        if(lo*lo + hi*hi >c) hi--;
+        else lo++;
+     }   
+     return false;
     }
 };
