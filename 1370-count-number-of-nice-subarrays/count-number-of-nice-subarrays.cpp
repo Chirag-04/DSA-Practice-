e8f3a8cb1468eea638d46a2b3464bf56af6cecb5
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-22 23:05:10 +0530

    Time: 172 ms (12.21%) | Memory: 93.9 MB (15.50%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& v, int k) {
-        vector<int>nums;
-        for(int i=0 ; i<v.size() ; i++){
-            if(v[i] % 2 != 0) nums.push_back(1);
-            else nums.push_back(0);
-        }
-        // now this problem is reduced to prefix su
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int,int>m;
         int count = 0 ;
         int sum = 0 ;
-        // vector<int>prefix(nums.size());
-        // prefix[0] = nums[0];
-        // for(int i=1 ; i<nums.size() ; i++){
-        //     prefix[i] = nums[i] + nums[i-1];
-        // }
-
-        for(int i=0 ; i<nums.size() ; i++){
-           sum+=nums[i];
-           if(sum==k) count++;
-           // if sum ==k  then also increase the counter 
-           
-            if(m.find(sum- k) != m.end() ){
-                //count+=1; // absolutely not
+        for(int i=0 ; i<n ; i++){
+            if(nums[i] % 2 != 0) sum++;
+            if(sum == k) count++;
+            if(m.find(sum -k)!=m.end()){
                 count+=m[sum-k];
             }
-           m[sum]++;
-
+            m[sum]++;
         }
-       
-        
-   return count ;
-
-
-
-
+        return count;
     }
 };
