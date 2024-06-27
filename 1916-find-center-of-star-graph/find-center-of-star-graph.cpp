d174e353c80e4a79f0d240b50018180dfcd3d4a6
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-27 19:45:20 +0530

    Time: 134 ms (67.07%) | Memory: 70.6 MB (92.91%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        vector<int>inDegree(edges.size()+2);
-        int center = -1;
-        for(auto it : edges){
-            int a = it[0];
-            int b = it[1];
-            inDegree[a]++;
-            inDegree[b]++;
-            if(inDegree[a] ==edges.size()) center = a;
-            if(inDegree[b] ==edges.size()) center = b;
-
-        }
-       return center;
-
+        // and 
+        if(edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]) return edges[0][0];
+        return edges[0][1];
     }
 };
