0b809efb0881ef5a3cb693a30f087d485bfd2d24
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-06-28 13:18:39 +0530

    Time: 92 ms (53.73%) | Memory: 23.6 MB (47.40%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    using PPI = pair< int , pair<int ,int>>;
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+        priority_queue<PPI , vector<PPI> , greater<PPI>>pq;
+        pq.push({0  , {0 , 0}});
+        int delr[4] = {-1 , 0 , 1 , 0};
+        int delc[4] = {0  , 1  , 0 , -1};
+        vector<vector<int>>path(n , vector<int>(m , 1e9));
+        path[0][0] = 0 ;
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int r = pq.top().second.first;
+            int c=  pq.top().second.second;
+            pq.pop();
+       for(int i=0 ; i<4 ; i++){
+        int nr  =  r + delr[i];
+        int nc =  c+ delc[i];
+        if(nr>=0 and nr<n and nc>=0 and nc<m){
+            // we want max diff
+            int newEffort =  max(dis , abs(heights[nr][nc] - heights[r][c]) );
+            if(newEffort < path[nr][nc]){
+                path[nr][nc] =  newEffort;
+            pq.push({ newEffort  ,{nr , nc}});
+            }
+        }
+       }     
+        }
+
+        return path[n-1][m-1];
+    }
+};
