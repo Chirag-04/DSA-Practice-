694b216ceadfa3e99c0823d2981f1aa8397cd2ff
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-07-19 07:43:52 +0530

    Time: 58 ms (32.39%) | Memory: 48.5 MB (31.11%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,81 @@
+class Trie {
+public:
+    struct trieNode{
+        bool isEndofWord ;
+        trieNode* children[26];
+        public:
+        trieNode(){
+            this->isEndofWord = false;
+            for(int i=0 ; i<26 ; i++){
+            this->children[i] = NULL;
+             }
+        }
+    };
+    trieNode* getNode(){
+        trieNode* newNode =  new trieNode();
+        return newNode;
+    }
+    trieNode* root;
+    Trie() {
+        root =  getNode();
+    }
+    
+    void insert(string word) {
+        // let say we want to insert "apple"
+        trieNode* crawler =  root;
+        int i=0 ;
+        for( ;i<word.size() ; i++){
+            char ch =  word[i];
+            int idx =  ch-'a';
+            if(crawler->children[idx] == NULL){
+                // no reference trie 
+                   crawler->children[idx] = getNode();
+            }
+            crawler =   crawler->children[idx] ;
+        }
+        // at last at the end of word
+        crawler->isEndofWord = true;
+    }
+    
+    bool search(string word) {
+        trieNode* crawler =  root;
+        int i= 0;
+        for( ; i<word.size() ; i++){
+            char ch = word[i];
+            int idx =  ch-'a';
+            if(crawler->children[idx] == NULL){
+                // no reference trie 
+                return false;
+            }
+            crawler =   crawler->children[idx] ;
+        }
+        // at last at the end of word
+       if(crawler != NULL and crawler-> isEndofWord == true) return true;
+       return false;
+    }
+    
+    bool startsWith(string word) {
+         trieNode* crawler =  root;
+        int i= 0;
+        for( ; i<word.size() ; i++){
+            char ch = word[i];
+            int idx =  ch-'a';
+            if(crawler->children[idx] == NULL){
+                // no reference trie 
+                return false;
+            }
+            crawler =   crawler->children[idx] ;
+        }
+        // at last at the end of word
+       if( i == word.size()) return true;
+       return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
