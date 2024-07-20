774b9e5ed4c178c453fc3fa7563724618a12298f
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-07-20 07:47:21 +0530

    Time: 3 ms (70.41%) | Memory: 11.9 MB (11.07%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,15 +1,62 @@
+class TrieNode{
+    public:
+    bool isEnd;
+    TrieNode* children[26];
+    int childCount;
+    TrieNode(){
+        this->isEnd  = false;
+        this->childCount  = 0;
+        for(int i=0; i<26 ; i++){
+            this->children[i] = NULL;
+        }
+    }
+};
+TrieNode* getNode(){
+    TrieNode* new_node = new TrieNode();
+    return new_node;
+}
+class Trie{
+    TrieNode* root;
+    public:
+    Trie(){
+        root = getNode();
+    }
+
+    void insert(string word){
+        TrieNode* crawler = root;
+        int i=0 ;
+        for(; i <word.size() ; i++){
+            int idx =  word[i] - 'a';
+            if(crawler->children[idx] == NULL){
+               crawler->children[idx] =  getNode();  
+               crawler->childCount++;  
+            }
+            crawler = crawler->children[idx];
+        }
+        crawler->isEnd = true;
+    }
+
+    void lcp(string first , string&ans){
+         TrieNode* crawler = root;
+        for(int i=0 ; i<first.size() ; i++){
+            int idx = first[i] -'a';
+            if(crawler->children[idx] && crawler->childCount == 1 && !crawler->isEnd){
+                ans.push_back(first[i]);
+            }else break;
+            crawler = crawler->children[idx];
+        }
+    }
+};
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // i will pick the first string 
-        string ans= "";
-        for(int i=0; i <strs[0].size() ; i++){
-            char ch =  strs[0][i];
-            for(int j=1 ; j<strs.size() ; j++){
-                if(ch != strs[j][i]) return ans;
-            }
-            ans+=ch;
+        Trie* t = new Trie();
+        for(int i=0 ; i<strs.size();  i++){
+            t->insert(strs[i]);
         }
+        string first = "";
+        string ans = "";
+        t->lcp(strs[0] , ans);
         return ans;
     }
 };
