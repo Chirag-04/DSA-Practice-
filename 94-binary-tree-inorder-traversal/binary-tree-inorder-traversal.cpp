6cb87125f79103b7c2710816ca7f252d707ca1fe
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-07-29 18:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (25.84%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -15,34 +15,71 @@ public:
     //using morris traversal
     // just check whether left node exist or not
     //
-    vector<int> inorderTraversal(TreeNode* root) {
-        if(root == NULL) return {};
+    vector<int>f(TreeNode* root){
         vector<int>ans;
         while(root){
             if(!root->left){
+                // traverse the left subtree 
                 ans.push_back(root->val);
-                root=root->right;
-            }
-            else{
+                root= root->right;
+            }else{
+                // rooot's left exists 
+                // now check whether there is a link or not 
                 TreeNode* curr = root->left;
-                while(curr->right!=NULL and curr->right!=root){
-                    curr= curr->right;
+                while(curr->right and curr->right !=root){
+                    curr = curr->right;
                 }
-                // 
+                // if curr->right == NULL then create a thread
                 if(curr->right == NULL){
-                   curr->right = root;
-                   root = root->left; 
-                }
-                else{
-                    // left root traversed 
-                    // now it's time to push the root nod e
-                    curr->right = NULL;
+                    curr->right = root;
+                    root= root->left;
+                }else{
+                    // loop exist
+                    // left side traversed 
                     ans.push_back(root->val);
+                    curr->right = NULL;
                     root= root->right;
                 }
             }
         }
+        return ans;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == NULL) return {}; // return null;
+        return f(root);
+    //    vector<int>ans;  // 
+        // while(root){
+        //     // if teh root->left dos not exist 
+        //     // we ahve completed the left subtree travers al
+        //     // print the root 
+        //     if(!root->left){
+        //         ans.push_back(root->val); // into  my answer 
+        //         root=root->right;
+        //     }
+        //     else{
+        //         TreeNode* curr = root->left; // to check the thread 
+        //         while(curr->right!=NULL and curr->right!=root){
+        //             curr= curr->right;
+        //         }
+        //         // i need to check 
+        //         // if curr ->right  = null then i need to create a thread 
+        //         // but if not i need to break the thread adm move to the right 
+        //         if(curr->right == NULL){ 
+        //            curr->right = root;
+        //            root = root->left; 
+        //         }
+        //         else{
+        //             // left root traversed 
+        //             // now it's time to push the root nod e
+        //             // theere is a thread and left subtree has been traverse 
+        //             // so break the thread and print ht root value 
+        //             curr->right = NULL;
+        //             ans.push_back(root->val);
+        //             root= root->right;
+        //         }
+        //     }
+        // }
 
-        return ans ;
+        // return ans ;
     }
 };
