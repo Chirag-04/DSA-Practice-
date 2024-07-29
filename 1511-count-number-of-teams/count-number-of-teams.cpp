65b7fd4fa721cb041ad769e49b166fff8ae67f5f
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-07-29 20:02:34 +0530

    Time: 41 ms (53.21%) | Memory: 11.6 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -3,14 +3,31 @@ public:
     int numTeams(vector<int>& rating) {
         int n = rating.size();
         int ans = 0;
-        for(int i=0  ; i<n ; i++){
-            for(int j=i+1 ; j<n ; j++){
-                for(int k=j+1 ;k<n ; k++){
-                    if((rating[i] < rating[j] and 
-                    rating[j] < rating[k]) or (rating[i] > rating[j] and 
-                    rating[j] > rating[k]) ) ans++;
-                }
+        for(int mid = 0 ;mid<n ; mid++){
+            // lets search for the left small
+            int left_ct = 0;
+            for(int left = 0 ; left<mid ; left++){
+                if(rating[left] < rating[mid]) left_ct++;
             }
+            int right_ct = 0;
+            for(int right = mid+1 ; right<n; right++){
+                if(rating[right] > rating[mid]) right_ct++;
+            }
+            ans+=(left_ct * right_ct);
+        }
+
+
+        for(int mid = 0 ;mid<n ; mid++){
+            // lets search for the left small
+            int left_ct = 0;
+            for(int left = 0 ; left<mid ; left++){
+                if(rating[left] > rating[mid]) left_ct++;
+            }
+            int right_ct = 0;
+            for(int right = mid+1 ; right<n; right++){
+                if(rating[right] < rating[mid]) right_ct++;
+            }
+            ans+=(left_ct * right_ct);
         }
         return ans ;
     }
